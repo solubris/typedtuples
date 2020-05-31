d7ec059dd4763d807df2e82d1d46e7738ced08f8
lithium147 <timlwalters@yahoo.co.uk>
2020-05-31 20:02:42 +0100

    enrichment example and SingleTest

---
+++ src/test/java/com/solubris/typedtuples/SingleTest.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2020 Solubris Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.solubris.typedtuples;
+
+import com.solubris.typedtuples.immutable.ImmutableTuple;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ *
+ */
+public class SingleTest {
+
+    @Test
+    void compute() {
+        var underTest = ImmutableTuple.of(0);
+        assertThat(underTest.get())
+                .isEqualTo(0);
+        assertThat(underTest.duplicate())
+                .isEqualTo(ImmutableTuple.of(0, 0));
+        assertThat(underTest.map(i -> i + 1))
+                .isEqualTo(ImmutableTuple.of(1));
+        assertThat(underTest.map(i -> i + 1))
+                .isEqualTo(ImmutableTuple.of(1));
+        assertThat(underTest.remove())
+                .isEqualTo(ImmutableTuple.of());
+        assertThat(underTest.add("x"))
+                .isEqualTo(ImmutableTuple.of(0, "x"));
+        assertThat(underTest.addFirst("x"))
+                .isEqualTo(ImmutableTuple.of("x", 0));
+        assertThat(underTest.replace("replace"))
+                .isEqualTo(ImmutableTuple.of("x"));
+        assertThat(underTest)
+                .isEqualTo(ImmutableTuple.of("0"));
+    }
+
+}
---
+++ src/test/java/com/solubris/typedtuples/examples/Enrichment.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2020 Solubris Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.solubris.typedtuples.examples;
+
+import com.solubris.typedtuples.immutable.ImmutableTuple;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Use tuples to enrich the items processed in the stream
+ * without having to create intermediary objects
+ */
+public class Enrichment {
+
+    @Test
+    void determineStringAttributes() {
+        List<StringStats> result = Stream.of("abc", "1234", "zzz")
+                .map(ImmutableTuple::of)
+                .map(s -> s.add(s.get().length()))
+                .map(sl -> sl.add(isPalindrome(sl.getFirst())))
+                .map(slp -> new StringStats(slp.getFirst(), slp.getSecond(), slp.get()))
+                .collect(Collectors.toList());
+        System.out.println(result);
+
+        assertThat(result)
+                .usingFieldByFieldElementComparator()
+                .contains(
+                        new StringStats("abc", 3, false),
+                        new StringStats("1234", 4, false),
+                        new StringStats("zzz", 3, true)
+                );
+    }
+
+    private boolean isPalindrome(String first) {
+        for (int i = 0; i < first.length() / 2; i++) {
+            if(first.charAt(i) != first.charAt(first.length() - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static class StringStats {
+        private final String value;
+        private final int length;
+        private final boolean palindrome;
+
+        public String getValue() {
+            return value;
+        }
+
+        public int getLength() {
+            return length;
+        }
+
+        public boolean isPalindrome() {
+            return palindrome;
+        }
+
+        private StringStats(String value, int length, boolean palindrome) {
+            this.value = value;
+            this.length = length;
+            this.palindrome = palindrome;
+        }
+    }
+}
